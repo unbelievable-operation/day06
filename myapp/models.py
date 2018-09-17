50b2d0bbb24b208bc2ed2a2ec6813cd29cd007c2
LPP <l.piaopiao@qq.com>
2018-09-17 17:39:09 +0800

    1.登录校验 2.cookie状态保持

---
+++ myapp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=10, unique=True,
+                                verbose_name='用户名')
+    password = models.CharField(max_length=255, unique=True,
+                                verbose_name='密码')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    operate_time = models.DateTimeField(auto_now=True, verbose_name='修改时间')
+
+    class Meta:
+        db_table = 'users'
+
+
+class UserTicket(models.Model):
+    user = models.ForeignKey(Users)
+    ticket = models.CharField(max_length=30)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        db_table = 'user_ticket'
+
